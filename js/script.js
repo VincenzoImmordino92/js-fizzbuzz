54f3773edf9c919eafc8fcf25433d20cf64e2e6b
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-09-11 17:14:18 +0200

    [fix] message

--- js/script.js
+++ js/script.js
@@ -2,12 +2,11 @@
 // 2. applichiamo il ciclo for per creare i box all'interno del nostro container
 // 3. aggiungiamo un'altra variabile dentro il for creando l'elemento div dentro il nostro box  
 // 4. aggiungiamo alla nostra variabile una classe di css creata appositamente per il box
-
+// 5.
 const caseContainer =  document.querySelector('.case');
+const bigContainer =   document.querySelector('.container');
+const message =        document.getElementById('output-message');
 
-const message = document.getElementById('output-message');
-
-caseContainer.classList.add('bg-blue-container');
 
 for( let i = 1 ; i <= 100 ;  i++ ){
 
@@ -31,14 +30,18 @@ for( let i = 1 ; i <= 100 ;  i++ ){
     box.classList.add('bg-green');
     box.innerHTML = ('Fizz');
   
-    } else if(( i % 5 == 0 )){
+  }else if(( i % 5 == 0 )){
       
     box.classList.add('bg-yellow');
     box.innerHTML = ('Buzz');
   }
   
 }
-message.innerHTML = 'Facciamo respirare questo codice'.toUpperCase();
-message.style.cssText ='fontWeight: bold;';
+
+message.innerHTML = 'FizzBuzzdom'.toUpperCase();
+
+//message.style.fontWeight = '700'
+// metodica old perchè aggiunge gli stili in html ma non è sbagliato
+message.style = 'color:white; font-size: 4rem;'
 
 
