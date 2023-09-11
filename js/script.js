8dfc8efe900fceaa99bbd66807391561fe85a6fa
vincenzoimmordino92 <vincenzoimmordino92@gmail.com>
2023-09-11 16:34:00 +0200

    [add] if

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@
 // 3. aggiungiamo un'altra variabile dentro il for creando l'elemento div dentro il nostro box  
 // 4. aggiungiamo alla nostra variabile una classe di css creata appositamente per il box
 
-const caseContainer = document.querySelector('.case');
+const caseContainer =  document.querySelector('.case');
+
+const message = document.getElementById('output-message');
 
 caseContainer.classList.add('bg-blue-container');
 
-for(let i = 1; i <= 100; i++){
+for( let i = 1 ; i <= 100 ;  i++ ){
 
-  let box = document.createElement('div');
+  let box =            document.createElement('div');
 
   box.classList.add('box');
 
@@ -18,6 +20,25 @@ for(let i = 1; i <= 100; i++){
   console.log(i,'box');
 
   caseContainer.append(box);
+  
+  if(( i % 3 == 0 ) && ( i % 5 == 0 )){
+  
+    box.classList.add('bg-red');
+    box.innerHTML = ('FizzBuzz');
+  
+  }else if(( i % 3 == 0 )){
+      
+    box.classList.add('bg-green');
+    box.innerHTML = ('Fizz');
+  
+    } else if(( i % 5 == 0 )){
+      
+    box.classList.add('bg-yellow');
+    box.innerHTML = ('Buzz');
+  }
+  
 }
+message.innerHTML = 'Facciamo respirare questo codice'.toUpperCase();
+message.style.cssText ='fontWeight: bold;';
 
 
